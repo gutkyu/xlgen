a82d87dabab412e3d545a86f025ac2e578fb2682
sangkyu jung <whoweboy@gmail.com>
2014-07-09 18:08:28 +0900

    modified test.js

--- test/test.js
+++ test/test.js
@@ -6,24 +6,27 @@ var xlg = xl.createXLGen('./test.xls');
 var fmtDate0 = xlg.addFormat(xl.formatStrings.date0);
 var fmtDate1 = xlg.addFormat(xl.formatStrings.date1);
 var fmtDate2 = xlg.addFormat(xl.formatStrings.date2);
-var custfmtDate = xlg.addFormat('0.0000E+00');
+var custfmtNum = xlg.addFormat('0.0000E+00');
 
-var sht = xlg.addSheet('Sheet1');
+var sht1 = xlg.addSheet('Sheet1');
+var sht2 = xlg.addSheet('Sheet2');
 //var sht = xlg.addSheet('시트1');
 
 console.log(xl.formatStrings);//print all standard format strings
 
 try{
-    sht.cell(0, 1, 1);
-    sht.cell(1, 1, 2.1);
-    sht.cell(2, 3, 'hi!');
-    sht.cell(3, 3, '안녕!');
-    sht.cell(5, 4, new Date);
-    sht.cell(6, 4, new Date, fmtDate0);
-    sht.cell(7, 4, 10000,fmtDate0);
-    sht.cell(8, 4, new Date, fmtDate1);
-    sht.cell(9, 4, new Date, fmtDate2);
-    sht.cell(10, 4, 9999,custfmtDate);
+    sht1.cell(0, 1, 1);
+    sht1.cell(1, 1, 2.1);
+    sht1.cell(2, 3, 'hi!');
+    sht1.cell(3, 3, '안녕!');
+	sht1.cell(10, 4, 9999,custfmtNum);
+	
+    sht2.cell(5, 4, new Date);
+    sht2.cell(6, 4, new Date, fmtDate0);
+    sht2.cell(7, 4, 10000,fmtDate0);
+    sht2.cell(8, 4, new Date, fmtDate1);
+    sht2.cell(9, 4, new Date, fmtDate2);
+    
 }catch(e){
     console.log(e.name, e.message);
 }
